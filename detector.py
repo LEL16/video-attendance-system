8eb0adf17d40643b5241d822e4fa55844c497bd0
LEL16 <elvitigalalis@gmail.com>
2024-05-02 12:22:01 -0400

    Base implementation with sample images.

---
+++ detector.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import face_recognition
+
+DEFAULT_ENCODING_PATH = Path("output/encodings.pkl")
+
+Path("training").mkdir(exist_ok=True)
+Path("output").mkdir(exist_ok=True)
+Path("validation").mkdir(exist_ok=True)
+
+def encode_known_faces(
+        model: str = "hog", encoding_location: Path = DEFAULT_ENCODING_PATH
+) -> None:
+    names = []
+    encodings = []
+    for filepath in Path("training").glob("*/*"):
+        name = filepath.parent.name
+        image = face_recognition.load_image_file(filepath)
