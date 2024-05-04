c18447ebbcd8c190aa879db8f8283fcde815731e
LEL16 <elvitigalalis@gmail.com>
2024-05-04 10:07:18 -0400

    Fixing dlib errors

--- detector.py
+++ detector.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import face_recognition
+import pickle
+from collections import Counter
 
 DEFAULT_ENCODING_PATH = Path("output/encodings.pkl")
 
@@ -8,10 +10,46 @@ Path("output").mkdir(exist_ok=True)
 Path("validation").mkdir(exist_ok=True)
 
 def encode_known_faces(
-        model: str = "hog", encoding_location: Path = DEFAULT_ENCODING_PATH
+        model: str = "hog", encodings_location: Path = DEFAULT_ENCODING_PATH
 ) -> None:
     names = []
     encodings = []
     for filepath in Path("training").glob("*/*"):
         name = filepath.parent.name
         image = face_recognition.load_image_file(filepath)
+
+        face_locations = face_recognition.face_locations(image, model=model)
+        face_encodings = face_recognition.face_encodings(image, face_locations)
+
+        for encoding in face_encodings:
+            names.append(name)
+            encodings.append(encoding)
+
+        name_encodings = {"names": names, "encodings": encodings}
+        with encodings_location.open(mode="wb") as f:
+            pickle.dump(name_encodings, f)
+
+    encode_known_faces()
+
+def recognize_faces(
+        image_location: str,
+        model: str = "hog",
+        encodings_location: Path = DEFAULT_ENCODING_PATH,
+) -> None:
+    with encodings_location.open(mode="rb") as f:
+        loaded_encodings = pickle.load(f)
+    
+    input_image = face_recognition.load_image_file(image_location)
+
+    input_face_locations = face_recognition.face_locations(image_location)
+
+    image_face_locations = face_recognition.face_locations(input_image, model=model)
+    input_face_encodings = face_recognition.face_encodings(input_image, input_face_locations)
+
+    for bounding_box, unknown_encoding in zip(input_face_locations, input_face_encodings):
+        name = _recognize_face(unknown_encoding, loaded_encodings)
+        if not name:
+            name = "Unknown"
+        print(name, bounding_box)
+
+
