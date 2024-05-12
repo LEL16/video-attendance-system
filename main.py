17b2fdad46227641f84ebb2476218a1d9ea9eb3b
9016Lysol <elvitigalalis@gmail.com>
2024-05-11 20:13:23 -0400

    Integrated modes into the GUI

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+import os
 
 cap = cv2.VideoCapture(0) # Starts video capture with my camera (0).
 # cap.set(3, 640) # Width (in px)
@@ -6,8 +7,18 @@ cap = cv2.VideoCapture(0) # Starts video capture with my camera (0).
 
 imgBackground = cv2.imread("video-attendance-system/resources/background.png") # Background image GUI
 
+imgModeDirectoryPath = "video-attendance-system/resources/modes"
+imgModeList = [] # Iterated through modes directory to get all mode images
+imgModePathNameArray = os.listdir(imgModeDirectoryPath) # Gets all the file names and puts in array for modes
+for imgPath in imgModePathNameArray:
+    imgModeList.append(cv2.imread(os.path.join(imgModeDirectoryPath, imgPath)))
+
+print(len(imgModeList))
 while True: 
     success, img = cap.read()
+
+    imgBackground[162:162 + 480, 55:55 + 640] = img # Overlays the webcam in the GUI
+
     cv2.imshow("webcam", img) # Integrated webcam into background GUI
     cv2.imshow("face attendance", imgBackground) # Background for the GUI
     cv2.waitKey(1) # Wait-time in ms
