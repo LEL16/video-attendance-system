3446a03aef690e8b65ed14a7a11e49f4d5aa71c7
9016Lysol <elvitigalalis@gmail.com>
2024-05-11 21:26:50 -0400

    Fully added bounding box to detected faces & added face matching to main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import cv2
 import os
+import face_recognition
 import pickle
+import numpy as np
+import cvzone
 
 cap = cv2.VideoCapture(0) # Starts video capture with my camera (0)
 cap.set(3, 640) # Width (in px)
@@ -27,10 +30,23 @@ while True:
 
     imgResized = cv2.resize(img, (0, 0), None, 0.25, 0.25) # Resizes image to 1/4 to save computing power
     imgResized = cv2.cvtColor(imgResized, cv2.COLOR_BGR2RGB) # Converts image colors from BGR to RGB
-    
+
+    newFaceLocationCurrentFrame = face_recognition.face_locations(imgResized) # Gets the location of the face from the current frame
+    newFaceEncodingCurrentFrame = face_recognition.face_encodings(imgResized, newFaceLocationCurrentFrame) # Gets the encoding of the face in the location specified in the current frame
+
     imgBackground[162 : 162 + 480, 55 : 55 + 640] = img # Overlays the webcam in the GUI
     imgBackground[44 : 44 + 633, 808 : 808 + 414] = imgModeList[2]
 
-    cv2.imshow("webcam", img) # Integrated webcam into background GUI
+    for newFaceLocation, newFaceEncoding in zip(newFaceLocationCurrentFrame, newFaceEncodingCurrentFrame):
+        faceMatches = face_recognition.compare_faces(knownFaceEncodingList, newFaceEncoding) # Compares known face encodings to current frame encodings to return boolean
+        faceDistance = face_recognition.face_distance(knownFaceEncodingList, newFaceEncoding) # Calculate the distance from known encodings to current frame face encoding to return double
+        faceMatchIndex = np.argmin(faceDistance) # Returns the index of the known face with the lowest distance
+        if faceMatches[faceMatchIndex]:
+            y1, x2, y2, x1 = newFaceLocation
+            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4 # Rescales bounding box to original scale (not resized scale)
+            bbox = 55 + x1, 162 + y1, x2 - x1, y2 - y1 # Creates bounding box with face locations
+            imgBackground = cvzone.cornerRect(imgBackground, bbox, rt = 0)
+            
+
     cv2.imshow("face attendance", imgBackground) # Background for the GUI
     cv2.waitKey(1) # Wait-time in ms
